6648d3d1fc4922ed081158d02ad2e10646c6e159
Wenhui Feng <whfeng@CNwhfeng.local>
2018-05-17 14:18:43 +0800

    Add a bean

---
+++ src/main/java/net/winfield/demo/computer/sb/bean/Input.java
@@ -0,0 +1,35 @@
+package net.winfield.demo.computer.sb.bean;
+
+public class Input {
+
+    private String param1;
+
+    private String param2;
+
+    private String operator;
+
+    public String getParam1() {
+        return param1;
+    }
+
+    public void setParam1(String param1) {
+        this.param1 = param1;
+    }
+
+    public String getParam2() {
+        return param2;
+    }
+
+    public void setParam2(String param2) {
+        this.param2 = param2;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+}
